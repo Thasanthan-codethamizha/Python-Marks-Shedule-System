a6dda06e3e087933dba6527f1e079327721aaf63
Code Thamizha_ Tech With Thasa <65809390+Thasanthan-codethamizha@users.noreply.github.com>
2021-04-17 22:13:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+print("-------------------MARKS SHEDULE PROJECT USING PYTHON-----------------------")
+
 ict = 0
 python = 1
 javascript = 2
+m = set()
 
 
 def identify_grade(marks):
@@ -22,41 +25,61 @@ def identify_grade(marks):
 
 class Student:
     def __init__(self):
-        self.name = input("Enter The Name")
-        self.ictm = float(input("Enter The ICT Marks:"))
-        ict_grade = identify_grade(self.ictm)
-        self.pythonm = float(input("Enter The PYTHON Marks:"))
-        python_grade = identify_grade(self.pythonm)
-        self.javascriptm = float(input("Enter The JAVASCRIPT Marks:"))
-        javascript_grade = identify_grade(self.javascriptm)
+        self.name = input("Enter The Name : ")
+        self.ictm = float(input("Enter The ICT Marks :"))
+        self.pythonm = float(input("Enter The PYTHON Marks :"))
+        self.javascriptm = float(input("Enter The JAVASCRIPT Marks :"))
         self.total = self.ictm + self.pythonm + self.javascriptm
         self.average = self.total/3
+        m.add(self.ictm)
+        m.add(self.pythonm)
+        m.add(self.javascriptm)
+
         grade = []
-        grade.extend(range(ict, javascript))
+        grade.extend(range(0, 3))
         grade[ict] = identify_grade(self.ictm)
         grade[python] = identify_grade(self.pythonm)
         grade[javascript] = identify_grade(self.javascriptm)
+        self.acount = 0
+        self.bcount = 0
+        self.ccount = 0
+        self.scount = 0
+        self.wcount = 0
+        if "A" in grade:
+            self.acount = grade.count("A")
+        if "B" in grade:
+            self.bcount = grade.count("B")
+        if "C" in grade:
+            self.ccount = grade.count("C")
+        if "S" in grade:
+            self.scount = grade.count("S")
+        if "W" in grade:
+            self.wcount = grade.count("W")
 
-        self.acount = grade.index("A")
-        self.bcount = grade.index("B")
-        self.ccount = grade.index("C")
-        self.scount = grade.index("S")
-        self.wcount = grade.index("W")
 
-
-total_students = 2
+print(" ")
+total_students = int(input("Enter The Students Count : "))
 student = []
 student.extend(range(0, total_students))
 n = 0
 while total_students > 0:
+    print(" ")
     student[n] = Student()
     print(student[n].name, " ", student[n].ictm, " ",
-          student[n].pythonm, " ", student[n].javascriptm, " ", student[n].acount, " ", student[n].bcount, " ", student[n].ccount, " ", " ")
+          student[n].pythonm, " ", student[n].javascriptm, " ", student[n].total, " ", student[n].average, " ", student[n].acount, " ", student[n].bcount, " ", student[n].ccount, " ", student[n].scount, " ", student[n].wcount)
+    print("--------------------------------",
+          student[n].name, "COMPLETED", "-----------------------------------")
     n = n+1
     total_students = total_students-1
+print(" ")
+print("---------------------------------COMPLETED------------------------------------------")
 
+print("NAME  ", " ICT", " PYTHON", " JAVASCRIPT",
+      " TOTAL", " AVERAGE", "A", "B", "C", "S", "W")
 for x in student:
-    print(x.name, " ", x.ictm, " ", x.pythonm, " ", x.javascriptm)
+    print(x.name, " ", x.ictm, " ", x.pythonm, " ", x.javascriptm, " ", x.total, " ", x.average, " ",
+          x.acount, " ", x.bcount, " ", x.ccount, " ", x.scount, " ", x.wcount)
+print(" ")
 
 
 def highest(j):
@@ -66,29 +89,43 @@ def highest(j):
             if y.ictm > max_marks:
                 max_marks = y.ictm
                 max_student = y.name
+            elif y.ictm == max_marks:
+                max_marks = y.ictm
+                max_student = max_student+"," + y.name
     elif j == 1:
         max_marks = 0
         for y in student:
-            if y.ictm > max_marks:
+            if y.pythonm > max_marks:
                 max_marks = y.pythonm
                 max_student = y.name
+            elif y.pythonm == max_marks:
+                max_marks = y.ictm
+                max_student = max_student+"," + y.name
     elif j == 2:
         max_marks = 0
         for y in student:
-            if y.ictm > max_marks:
+            if y.javascriptm > max_marks:
                 max_marks = y.javascriptm
                 max_student = y.name
+            elif y.javascriptm == max_marks:
+                max_marks = y.ictm
+                max_student = max_student+"," + y.name
 
     return max_student, max_marks
 
 
+print(" ")
+print("HIGHSEST MARKS ON THE SUBJECTS")
 ictmaxstudent, ictmaxmarks = highest(0)
-print("THE HIGHEST MARKS ON ICT IS ", ictmaxmarks, "GET BY", ictmaxstudent)
+print("ICT :- ", ictmaxmarks, "GET BY", ictmaxstudent)
 
 pythonmaxstudent, pythonmaxmarks = highest(1)
-print("THE HIGHEST MARKS ON PYTHON IS ",
+print("PYTHON :- ",
       pythonmaxmarks, "GET BY", pythonmaxstudent)
 
 javascriptmaxstudent, javascriptmaxmarks = highest(2)
-print("THE HIGHEST MARKS ON JAVASCRIPT IS ",
+print("JAVASCRIPT :- ",
       javascriptmaxmarks, "GET BY", javascriptmaxstudent)
+
+print(" ")
+print(m)
